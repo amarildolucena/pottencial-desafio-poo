ad6a27187f518fbda5460a28954d1bf66fbaf1f4
Amarildo Lucena <amarildolucena@gmail.com>
2022-11-21 09:35:27 -0300

    Desafio entregue.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Inicializando Android...");
+Smartphone android = new Nokia(numero: "84987651234", modelo: "TopLinhaAndroid", imei: "987654321", memoria: 256);
+android.InstalarAplicativo("Skype");
+android.Ligar();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Inicializando IOs...");
+Smartphone siri = new Iphone(numero: "84912349876", modelo: "TopLinhaIphone", imei: "123456789", memoria: 256);
+siri.InstalarAplicativo("Instagram");
+siri.ReceberLigacao();
